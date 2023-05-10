c92f0a0d027751fdb0f366360d1308ad4d619f9d
3ddelano <3ddelano@gmail.com>
2023-05-10 22:08:30 +0530

    update build workflow

--- sample/addons/discord-game-sdk-godot/export_plugin.gd
+++ sample/addons/discord-game-sdk-godot/export_plugin.gd
@@ -5,11 +5,14 @@
 @tool
 extends EditorExportPlugin
 
+func _get_name() -> String:
+	return "Discord Game SDK Godot"
 
 func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int):
 	print("\n\n----------------")
 	printt(features)
 	var target_platform = features[2]
+
 	match target_platform:
 		"windows":
 			add_shared_object("res://addons/discord-game-sdk-godot/bin/discord_game_sdk.dll", [], "/")
